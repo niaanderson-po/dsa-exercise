f5ef594ac3d0281661a612f5d040652ee563c3ec
niaanderson-po <niaanderson.po@gmail.com>
2025-01-30 23:56:30 -0500

    feat: 200. Number of Islands

---
+++ queue & stack/200. Number of Islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visit = set()
+        islands = 0
+
+        def bfs(r,c):
+            queue = deque()
+            visit.add((r,c))
+            queue.append((r,c))
+            while queue:
+                row, col = queue.popleft()
+                direction = [[1,0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in direction:
+                    r,c = row + dr, col + dc
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == "1" and 
+                        (r,c) not in visit):
+                        queue.append([r,c])
+                        visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    islands += 1
+        return islands
